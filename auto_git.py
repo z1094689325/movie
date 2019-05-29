d28053a1e103a35ec0bbcc46d308271b1f62cb16
unknown <375333771@qq.com>
2019-05-29 15:57:10 +0800

    重新构造了auto_git.py,使得它看起来更正规

--- auto_git.py
+++ auto_git.py
@@ -3,11 +3,19 @@
 import os
 import sys
 
-os.system('git add .')
 
-os.system('git commit -m "{}"'.format(sys.argv[1]))
+def auto_git(m):
 
-os.system('git push https://gitee.com/zhangpengju/movie_resource_station_search master')
+    os.system('git add .')
+
+    os.system('git commit -m "{}"'.format(m))
+
+    os.system('git push https://gitee.com/zhangpengju/movie_resource_station_search master')
+
+
+if __name__ == "__main__":
+
+    auto_git(sys.argv[1])
 
 
 
